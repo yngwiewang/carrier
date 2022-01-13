8b694bcbd9bb5746f3200136296e3c375a22f9da
yngwiewang <yngwiewang@163.com>
2022-01-13 16:48:01 +0800

    feature: 1. Split result into stdout and stderr 2. Add "SN" to `carrier logs` 3. Change execute duration from float64 to time.Duration 4. Save result Gob file to /tme/carrier_record

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 build:
 	go build cmd/carrier.go
+
+install:
+	go install cmd/carrier.go
---
+++ pkg/host/ssh.go
@@ -0,0 +1,49 @@
+package host
+
+import (
+	"net"
+	"time"
+
+	"golang.org/x/crypto/ssh"
+)
+
+// Conn wraps a net.Conn, and sets a deadline for every read
+// and write operation.
+type Conn struct {
+	net.Conn
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+// Implementation of io.Reader with timeout.
+func (c *Conn) Read(b []byte) (int, error) {
+	err := c.Conn.SetReadDeadline(time.Now().Add(c.ReadTimeout))
+	if err != nil {
+		return 0, err
+	}
+	return c.Conn.Read(b)
+}
+
+// Implementation of io.Writer.
+func (c *Conn) Write(b []byte) (int, error) {
+	err := c.Conn.SetWriteDeadline(time.Now().Add(c.WriteTimeout))
+	if err != nil {
+		return 0, err
+	}
+	return c.Conn.Write(b)
+}
+
+func sshDialTimeout(network, addr string, config *ssh.ClientConfig, timeout time.Duration) (*ssh.Client, error) {
+	conn, err := net.DialTimeout(network, addr, timeout)
+	if err != nil {
+		return nil, err
+	}
+
+	timeoutConn := &Conn{conn, timeout, timeout}
+	c, chans, reqs, err := ssh.NewClientConn(timeoutConn, addr, config)
+	if err != nil {
+		return nil, err
+	}
+	client := ssh.NewClient(c, chans, reqs)
+	return client, nil
+}
